9570dd85892cc6389c5b9df2280d1bcf43bd5500
youssef <benhassine.qlio@gmail.com>
2026-08-21 19:17:20 +0200

    implemented LRU to buffer pool

--- src/buffer_pool.c
+++ src/buffer_pool.c
@@ -7,11 +7,13 @@ typedef struct {
   int page_id;
   int valid;
   int dirty;
+  unsigned long last_used;
 } Frame;
 
 struct BufferPool {
   PageManager *manager;
   Frame frames[BUFFER_POOL_SIZE];
+  unsigned long clock;
 };
 
 BufferPool *buffer_pool_create(PageManager *manager) {
@@ -24,11 +26,13 @@ BufferPool *buffer_pool_create(PageManager *manager) {
     return NULL;
 
   pool->manager = manager;
+  pool->clock = 0;
 
   for (int i = 0; i < BUFFER_POOL_SIZE; i++) {
     pool->frames[i].page_id = -1;
     pool->frames[i].valid = 0;
     pool->frames[i].dirty = 0;
+    pool->frames[i].last_used = 0;
   }
 
   return pool;
@@ -47,15 +51,22 @@ Page *buffer_pool_get(BufferPool *pool, int page_id) {
   if (!pool || page_id < 0)
     return NULL;
 
+  /*
+   * 1. check weather pgae is already cached
+   */
   for (int i = 0; i < BUFFER_POOL_SIZE; i++) {
     Frame *frame = &pool->frames[i];
 
-    if (frame->valid && frame->page_id == page_id)
+    if (frame->valid && frame->page_id == page_id) {
+      frame->last_used = pool->clock++;
       return &frame->page;
+    }
   }
 
+  /*
+   * 2. look for an unused frame
+   */
   for (int i = 0; i < BUFFER_POOL_SIZE; i++) {
-
     Frame *frame = &pool->frames[i];
 
     if (!frame->valid) {
@@ -65,10 +76,45 @@ Page *buffer_pool_get(BufferPool *pool, int page_id) {
       frame->page_id = page_id;
       frame->valid = 1;
       frame->dirty = 0;
+      frame->last_used = pool->clock++;
 
       return &frame->page;
     }
   }
+
+  /*
+   * 3. Pool is full
+   * Find the least recently used frame
+   */
+  int lru_index = 0;
+
+  for (int i = 1; i < BUFFER_POOL_SIZE; i++) {
+    if (pool->frames[i].last_used < pool->frames[lru_index].last_used)
+      lru_index = i;
+  }
+
+  Frame *frame = &pool->frames[lru_index];
+
+  /*
+   * 4. if page was modified
+   * write it back before replacing it
+   */
+  if (frame->dirty) {
+    if (!page_manager_write(pool->manager, frame->page_id, &frame->page))
+      return NULL;
+  }
+
+  /*
+   * 5. if page was modified
+   * write it back before replacing it
+   */
+  if (!page_manager_read(pool->manager, page_id, &frame->page))
+    return NULL;
+
+  frame->page_id = page_id;
+  frame->valid = 1;
+  frame->dirty = 0;
+  frame->last_used = pool->clock++;
 
   return NULL;
 }
